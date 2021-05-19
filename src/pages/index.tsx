6d491c49c2a1e3aa6a4e88768f0ab16041d40c2f
Samuel Ramos <samuelsantos7@yahoo.com.br>
2021-05-19 01:36:46 -0300

    Static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+
 import SubscribeButton from '../components/SubscribeButton'
 import { stripe } from '../services/stripe'
+
 import styles from '../styles/home.module.scss'
 
 interface HomeProps {
@@ -37,7 +39,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IsgX2DC3Xvw9Gs7B6hPsdde')
 
   const product = {
@@ -51,6 +53,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 horas
   }
 }
