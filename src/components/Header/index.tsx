ed11e84d67e60fccf980e71cfa0df2f0d37dddaf
Samuel Ramos <samuelsantos7@yahoo.com.br>
2021-05-19 00:06:36 -0300

    Componente de Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.scss'
+
+export const Header: React.FC = () => {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a href="/" className={styles.active}>Home</a>
+          <a href="/">Posts</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
